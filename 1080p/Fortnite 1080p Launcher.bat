5eec4acc44fb7e0f70262c9372f7708653c4ff62
orbitthegreatest <cacapasdetrou01@gmail.com>
2026-08-29 14:24:45 -0400

    Debug v3: call exe directly, redirect output to log

--- 1080p/Fortnite 1080p Launcher.bat
+++ 1080p/Fortnite 1080p Launcher.bat
@@ -3,8 +3,6 @@ title Fortnite 1080p Launcher
 setlocal enabledelayedexpansion
 
 set "LOG=%temp%\fortnite_1080p.log"
-echo === %date% %time% === > "%LOG%"
-
 set "SCRIPT_DIR=%~dp0"
 set "ASSET_DIR=%localappdata%\FortniteLaunchersAssets"
 set "RES_EXE=%ASSET_DIR%\SetResolution.exe"
@@ -15,9 +13,8 @@ set "DATA_DIR=C:\Users\tutot\Desktop\Orbit settings\Orbit settings\Data (DO NOT 
 set "SETTINGS_SRC=%DATA_DIR%\GameUserSettings.ini"
 set "SETTINGS_DEST=%localappdata%\FortniteGame\Saved\Config\WindowsClient\GameUserSettings.ini"
 
-echo A >> "%LOG%"
+echo === %date% %time% === > "%LOG%"
 if not exist "%ASSET_DIR%" mkdir "%ASSET_DIR%"
-echo B >> "%LOG%"
 if not exist "%ICON_PATH%" (
     set "LOCAL_ICONS=%SCRIPT_DIR%..\icons"
     if exist "!LOCAL_ICONS!\%ICON_NAME%" (
@@ -26,7 +23,6 @@ if not exist "%ICON_PATH%" (
         powershell -NoProfile -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri '%GITHUB_BASE%/icons/%ICON_NAME%' -OutFile '%ICON_PATH%' -UseBasicParsing"
     )
 )
-echo C >> "%LOG%"
 if not exist "%RES_EXE%" (
     set "LOCAL_EXE=%SCRIPT_DIR%..\SetResolution.exe"
     if exist "!LOCAL_EXE!" (
@@ -35,31 +31,34 @@ if not exist "%RES_EXE%" (
         powershell -NoProfile -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri '%GITHUB_BASE%/SetResolution.exe' -OutFile '%RES_EXE%' -UseBasicParsing"
     )
 )
-echo D >> "%LOG%"
-echo E src=%SETTINGS_SRC% >> "%LOG%"
-echo F dest=%SETTINGS_DEST% >> "%LOG%"
-echo G exist_src= >> "%LOG%"
-if exist "%SETTINGS_SRC%" (echo YES >> "%LOG%") else (echo NO >> "%LOG%")
-echo H exist_dest_folder= >> "%LOG%"
-if exist "%localappdata%\FortniteGame\Saved\Config\WindowsClient\" (echo YES >> "%LOG%") else (echo NO >> "%LOG%")
-echo I removing_readonly >> "%LOG%"
-if exist "%SETTINGS_DEST%" attrib -r "%SETTINGS_DEST%" >nul 2>&1
-echo J copying >> "%LOG%"
-copy /y "%SETTINGS_SRC%" "%SETTINGS_DEST%" >nul
-echo K copy_result=%errorlevel% >> "%LOG%"
-echo L setting_readonly >> "%LOG%"
-attrib +r "%SETTINGS_DEST%" >nul 2>&1
-echo M done_copying >> "%LOG%"
-echo N checking_exe >> "%LOG%"
-if exist "%RES_EXE%" (
-    echo O exe_found launching >> "%LOG%"
-    start "" "%RES_EXE%" 1500 1080 --launch
-    echo P launch_sent >> "%LOG%"
-) else (
-    echo O exe_not_found >> "%LOG%"
-    start "" /high "com.epicgames.launcher://apps/Fortnite?action=launch&silent=true"
-    echo P fallback_launch_sent >> "%LOG%"
+
+echo Applying GameUserSettings.ini...
+if not exist "%SETTINGS_SRC%" (
+    echo [ERROR] GameUserSettings.ini not found.
+    pause
+    exit /b 1
 )
-echo Q very_end >> "%LOG%"
-pause
+if not exist "%localappdata%\FortniteGame\Saved\Config\WindowsClient\" (
+    echo [ERROR] Fortnite config folder not found.
+    pause
+    exit /b 1
+)
+if exist "%SETTINGS_DEST%" attrib -r "%SETTINGS_DEST%" >nul 2>&1
+copy /y "%SETTINGS_SRC%" "%SETTINGS_DEST%" >nul
+if %errorlevel% equ 0 (
+    attrib +r "%SETTINGS_DEST%" >nul 2>&1
+    echo [OK] GameUserSettings.ini applied.
+) else (
+    echo [ERROR] Failed to copy GameUserSettings.ini.
+    pause
+    exit /b 1
+)
+
+echo Launching Fortnite 1080p...
+if exist "%RES_EXE%" (
+    "%RES_EXE%" 1500 1080 --launch >> "%LOG%" 2>&1
+) else (
+    start "" /high "com.epicgames.launcher://apps/Fortnite?action=launch&silent=true"
+)
+echo Done.
 endlocal
